0ed808d5feb899c626b1de05af2e200c22b17f1e
DEV_WOONG <ywcsuwon@gmail.com>
2024-12-18 21:55:19 +0900

    feat: 스프링으로 전환

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.config.AppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+    public static void main(String[] args) {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        System.out.println("new member = " + member.getName());
+    }
+}
--- src/main/java/hello/core/config/AppConfig.java
+++ src/main/java/hello/core/config/AppConfig.java
@@ -8,21 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * 어플리케이션 설정 정보
+ */
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
-    private DiscountPolicy discountPolicy() {
+    
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//      return new FixDiscountPolicy
         return new RateDiscountPolicy();
     }
 }
