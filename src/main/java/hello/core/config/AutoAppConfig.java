d43c072c0a49501b0161bb6fc0836448d8939310
DEV_WOONG <ywcsuwon@gmail.com>
2025-01-27 23:06:27 +0900

    강의내용 실습

--- src/main/java/hello/core/config/AutoAppConfig.java
+++ src/main/java/hello/core/config/AutoAppConfig.java
@@ -2,9 +2,13 @@ package hello.core.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan
+@ComponentScan(
+        basePackages = "hello.core",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+)
 public class AutoAppConfig {
-    
+
 }
