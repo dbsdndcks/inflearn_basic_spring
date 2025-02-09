59bf536df1094f332eef0fdad7b61314b32b4f24
DEV_WOONG <ywcsuwon@gmail.com>
2025-02-09 22:32:25 +0900

    스프링 콜백 추론기능

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -32,17 +32,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존관계 주입 끝나면 호출해주겠다
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화연결 메시지");
     }
 
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
